5a9dd6f8bcf27e30c0d376fc8a02449867ea272f
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-03-31 12:49:06 -0300

    fix: update favicon.png to a new icon

---
+++ gatsby-config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  siteMetadata: {
+    siteTitle: `Mimitsu`,
+    defaultTitle: `Mimitsu | Documentation`,
+    siteTitleShort: `Mimitsu`,
+    siteDescription: `A focused RPG discord bot which brings a custom farm system and also, a lot of fun.`,
+    siteUrl: `localhost:8000`,
+    siteAuthor: `@Loliticos`,
+    siteImage: `/banner.png`,
+    siteLanguage: `en`,
+    themeColor: `#c696e2`,
+    basePath: `/`,
+    footer: `Copyright © 2020 Loliticos. All rights Reserved`
+  },
+  plugins: [
+    {
+      resolve: `@rocketseat/gatsby-theme-docs`,
+      options: {
+        configPath: `src/config`,
+        docsPath: `src/docs`,
+        githubUrl: `https://github.com/rocketseat/gatsby-themes`,
+        baseDir: `docs/`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Mimitsu`,
+        short_name: `Mimitsu`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        display: `standalone`,
+        icon: `static/favicon.png`
+      }
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // trackingId: ``,
+      }
+    },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `localhost:8000`
+      }
+    },
+    `gatsby-plugin-offline`
+  ]
+};
